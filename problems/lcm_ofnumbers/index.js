7faf0491590f7b803094655fff95073aeccdb49d
mahitalk <mamtay0211@gmail.com>
2025-01-25 13:25:46 +0530

    LCM of Two Numbers

--- problems/lcm_ofnumbers/index.js
+++ problems/lcm_ofnumbers/index.js
@@ -1,10 +1,14 @@
 // LCM of Two Numbers
 function lcmOfNumbers(a, b) {
+  let arr = [];
   for (let i = 2; i <= a && i <= b; i++) {
     if (a % i == 0 && b % i == 0) {
-      return i;
+      arr.push(i);
     }
   }
+  return (a * b) / arr[arr.length - 1];
 }
-console.log(lcmOfNumbers(78, 221));
-console.log(lcmOfNumbers(7898, 78));
+console.log(lcmOfNumbers(4, 6));
+console.log(lcmOfNumbers(11, 22));
+console.log(lcmOfNumbers(2, 3)); // not working
+console.log(lcmOfNumbers(17, 2)); // not working
